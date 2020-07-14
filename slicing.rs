46560510722886de43799b1416c626fad2e5916a
upanshu <upanshu21@gmail.com>
2020-07-15 01:06:20 +0530

    example added

---
+++ slicing.rs
@@ -0,0 +1,38 @@
+fn main() {
+
+    //example1
+    let dummy = String::from("hello");
+
+    let slice = &dummy[0..2];
+
+    println!("{}", slice);
+
+
+    //method 2
+
+    let dummy_two = String::from("hello world");
+    let length = dummy_two.len();
+    let slice_two = &dummy_two[..length];
+
+    println!("{}", slice_two);
+
+
+    //example 
+
+    let a = first_word(&dummy_two);
+    println!("{}", a);
+
+}
+
+//program to find first word
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
